1fb169b09baf560e707df874a34f64a316f2ec08
Jimmy9007 <jimmy-9010@hotmail.com>
2026-06-04 14:47:12 -0500

    Configuración: mover IP a local.php y agregar local.php.dist como ejemplo

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,41 +1,18 @@
 <?php
 
-/**
- * Global Configuration Override
- *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
-//------------------------------------------------------------------------------
-/* $host = '190.90.224.252';
-$puerto = '6303'; */
-//$host = '10.220.1.100';
-//$puerto = '3306';
-//$servidor = $host . ':' . $puerto;
-//------------------------------------------------------------------------------
-$host = '10.220.1.100';
-$servidor = $host; 
-//------------------------------------------------------------------------------
 return [
     'db' => [
-        /* 'driver' => 'Pdo',
-        'dsn' => "mysql:dbname=gestorportal_bd;host=$servidor;charset=utf8", */
         'adapters' => [
             'gestorportal_bd' => [
                 'driver' => 'Pdo',
-                'dsn' => "mysql:dbname=test_gestor;host=$servidor",
+                'dsn' => "mysql:dbname=test_gestor;host=%HOST%",  # ← marcador
                 'driver_options' => [
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
                 ],
             ],
             'portal_db' => [
                 'driver' => 'Pdo',
-                'dsn' => "mysql:dbname=test_portal;host=$servidor",
+                'dsn' => "mysql:dbname=test_portal;host=%HOST%",  # ← marcador
                 'driver_options' => [
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
                 ],
